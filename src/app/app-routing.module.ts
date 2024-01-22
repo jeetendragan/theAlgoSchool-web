4706b94f6cd25017fb7951031a135c3ac7f4e6e7
Jeetendra Gan <jeetendra.gan@gmail.com>
2024-01-22 18:27:55 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,10 +10,6 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
     path: 'blog/:id',
     component: BlogComponent
   },
